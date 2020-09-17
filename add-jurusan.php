eb82d1cbc11fbae32b66f15e601a1d3712183313
kumpultugas17 <kumpultugas17@gmail.com>
2020-09-17 15:19:07 +0800

    Insert data dan Koneksi

---
+++ add-jurusan.php
@@ -0,0 +1,37 @@
+<?php 
+    include 'config/koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Jurusan</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="jur">Jurusan</label>
+        <input type="text" id="jur" name="jurusan">
+        <br>
+        <button type="submit" name="submit">Simpan</button>
+    </form>
+</body>
+</html>
+
+<?php 
+if (isset($_POST['submit'])) {
+    $jurusan = $_POST['jurusan'];
+
+    $result = "INSERT INTO jurusan (nama, create_at, update_at) VALUES ('$jurusan', '', '')";
+    $sql = mysqli_query($conn, $result);
+
+    if ($sql) {
+        echo "Data berhasil disimpan!";
+    } else {
+        echo "Data gagal disimpan!";
+    }
+    
+}
+
+?>
